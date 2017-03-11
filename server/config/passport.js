c5eaa25af78c7568b71a2d18d6a11d7c11422255
Venelin Petkov <vpetkov@obekto.bg>
2017-03-11 22:34:03 +0200

    Added local Strategy.

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,40 +2,40 @@ const passport = require("passport");
 const Strategy = require("passport-http-bearer").Strategy;
 
 module.exports = function() {
-    // passport.use(new Strategy(function(username, password, done) {
-    //     user.findOne({ username: username }).exec(function(err, user) {
-    //         if (err) {
-    //             console.log("Error loading user: " + err);
-    //             return;
-    //         }
-    //         if (user && user.authenticate(password)) {
-    //             return done(null, user);
-    //         } else {
-    //             return done(null, false);
-    //         }
-    //     })
-    // }));
-
-    passport.use(new Strategy((token, done) => {
-        data.users.findByToken(token)
-            .then((user) => {
-                if (!user) {
-                    return done(null, false);
-                }
-
-                return done(null, user, {
-                    scope: "all"
-                });
-            });
+    passport.use(new Strategy(function(username, password, done) {
+        user.findOne({
+            username
+        }).exec((err, user) => {
+            if (err) {
+                console.log("Error loading user: " + err);
+                return;
+            }
+            if (user && user.authenticate(password)) {
+                done(null, user);
+            } else {
+                done(null, false);
+            }
+        });
     }));
 
     passport.serializeUser((user, done) => {
-        done(null, user.id);
+        if (user) {
+            return done(null, user._id);
+        }
     });
 
-    passport.deserializeUser((token, done) => {
-        return data.users.findByToken(token)
-            .then((user) => done(null, user))
-            .catch((err) => done(err));
+    passport.deserializeUser(function(id, done) {
+        User.findOne({ _id: id }).exec(function(err, user) {
+            if (err) {
+                console.log("Error loading user: " + err);
+                return;
+            }
+
+            if (user) {
+                done(null, user);
+            } else {
+                done(null, false);
+            }
+        });
     });
 };
