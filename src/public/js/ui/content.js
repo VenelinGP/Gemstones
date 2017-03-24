afa7f1420cedf741053f6aec90b044a5498124b9
Venelin Petkov <vpetkov@obekto.bg>
2017-03-25 01:10:43 +0200

    Created to work navigattion in admin and user panels. Added favicon.ico.

--- src/public/js/ui/content.js
+++ src/public/js/ui/content.js
@@ -12,6 +12,9 @@
                     });
                     $(contentSelector).remove();
                     $("#content-wrap").html(html);
+                })
+                .then(() => {
+                    this.tabs();
                 });
         },
         // show() {
@@ -22,5 +25,26 @@
         //     $(contentSelector).addClass("hidden");
         //     return this;
         // },
+        tabs() {
+            $tabs = $("#info .tabs");
+            let $items = $tabs.find(">li>a");
+            // console.log("Tabs: " + $items.length);
+            let current = 0;
+            $panes = $("#info .main .pane");
+            // console.log("Panes: " + $panes.length);
+            $panes.hide();
+            $items.eq(current).addClass("active");
+            $panes.eq(current).show();
+            $items.click(function(ev) {
+                ev.preventDefault();
+                index = $items.index(this);
+                $panes.eq(current).fadeOut(function() {
+                    $panes.eq(index).fadeIn();
+                    current = index;
+                });
+            });
+            return false;
+
+        }
     };
 })(window);
